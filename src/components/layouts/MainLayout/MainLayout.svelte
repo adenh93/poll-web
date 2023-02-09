d9bf074d345fe2e4488c0b896ece832fa052cf1a
adenh93 <aden.herold1@gmail.com>
2023-02-09 20:07:37 +1100

    feat: added MainLayout component

---
+++ src/components/layouts/MainLayout/MainLayout.svelte
@@ -0,0 +1,26 @@
+<div class="container">
+  <div class="primary-section">
+    <slot name="primary" />
+  </div>
+  <div class="secondary-section">
+    <slot name="secondary" />
+  </div>
+</div>
+
+<style>
+  .container {
+    width: 100%;
+    padding: 0.2rem;
+  }
+
+  .primary-section {
+    background: var(--gradient-bg), url("/assets/images/background.jpg");
+    background-size: cover;
+    border-radius: 0.5rem;
+    padding: 10rem 3rem;
+  }
+
+  .secondary-section {
+    padding: 4rem;
+  }
+</style>
