d1a1f3c3c4b0507c1ab996851805abc26b45b998
adenh93 <aden.herold1@gmail.com>
2023-02-09 20:19:14 +1100

    feat: added TextArea UI component

---
+++ src/components/ui/TextArea/TextArea.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  export let name: string;
+  export let label: string;
+</script>
+
+<div class="input-wrapper">
+  <textarea {name} {...$$restProps} />
+  <label for={name}>{label}</label>
+</div>
+
+<style>
+  textarea {
+    padding: 1.8rem 2.5rem;
+    border-radius: 0.5rem;
+    border: 2px solid var(--border-color);
+    width: 100%;
+    box-sizing: border-box;
+    resize: none;
+  }
+
+  .input-wrapper > textarea:focus {
+    border: 2px solid var(--purple-light);
+    outline: 0;
+  }
+
+  .input-wrapper > textarea:focus + label {
+    color: var(--purple-light);
+  }
+</style>
