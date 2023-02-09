ee7290a5beb08fd5a73e54b0a56673b4317ebbfe
adenh93 <aden.herold1@gmail.com>
2023-02-09 20:20:27 +1100

    feat: added HomePage page component
    feat: added background image + icons to public dir

---
+++ src/components/pages/HomePage/HomePage.stories.ts
@@ -0,0 +1,12 @@
+import HomePage from './HomePage.svelte';
+
+export default {
+  component: HomePage,
+  title: "HomePage",
+};
+
+const Template = () => ({
+  Component: HomePage,
+});
+
+export const Default = Template.bind({});
---
+++ src/components/pages/HomePage/HomePage.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import MainLayout from "@app/components/layouts/MainLayout";
+  import PollForm from "@app/components/ui/PollForm";
+  import Steps from "@app/components/ui/Steps";
+</script>
+
+<MainLayout>
+  <div class="primary-container" slot="primary">
+    <h1>Create a Poll in a Few Easy Steps!</h1>
+    <div class="form-card">
+      <PollForm />
+    </div>
+  </div>
+  <div class="secondary-container" slot="secondary">
+    <h2>How It Works</h2>
+    <Steps />
+  </div>
+</MainLayout>
+
+<style>
+  h1 {
+    color: var(--text-white);
+    width: 90%;
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  .primary-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 3rem;
+    width: 59.6rem;
+    margin: 0 auto;
+  }
+
+  .form-card {
+    background-color: var(--bg-white);
+    border-radius: 0.5rem;
+    padding: 3em;
+  }
+
+  .secondary-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 5rem;
+  }
+</style>
