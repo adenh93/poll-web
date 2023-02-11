2b1a92d3186093c3aa6374d82637e79bac1012f8
adenh93 <aden.herold1@gmail.com>
2023-02-11 21:08:20 +1100

    feat: added FormGroup component

---
+++ src/components/ui/FormGroup/FormGroup.svelte
@@ -0,0 +1,24 @@
+<div class="input-wrapper">
+  <slot name="adornment-left" class="icon-left" />
+  <slot />
+  <slot name="adornment-right" class="icon-right" />
+  <slot name="errors" />
+</div>
+
+<style>
+  .input-wrapper {
+    position: relative;
+  }
+
+  :global([slot="adornment-left"]) {
+    position: absolute;
+    top: 0;
+    left: 1rem;
+  }
+
+  :global([slot="adornment-right"]) {
+    position: absolute;
+    top: 0;
+    right: 1rem;
+  }
+</style>
