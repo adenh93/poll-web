7e518716e7cb3bac5c199a7b13d34bc535f91fa0
adenh93 <aden.herold1@gmail.com>
2023-02-11 21:07:57 +1100

    feat: added IconButton component

---
+++ src/components/ui/IconButton/IconButton.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let ariaLabel: string;
+</script>
+
+<button on:click {...$$restProps} aria-label={ariaLabel}>
+  <slot />
+</button>
+
+<style>
+  button {
+    display: flex;
+    align-items: center;
+    vertical-align: middle;
+    background: none;
+    border: none;
+    padding: 1.5rem 0.8rem;
+    cursor: pointer;
+  }
+</style>
