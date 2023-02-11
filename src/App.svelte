e64bb4e6c34451362abc33bf46796fb486aa3d2d
adenh93 <aden.herold1@gmail.com>
2023-02-11 21:12:11 +1100

    feat: constructed svelte-query QueryClient and wrapped component tree in QueryClientProvider

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,12 @@
-<main>
-  <h1>Hello world!</h1>
-</main>
+<script lang="ts">
+  import { QueryClient, QueryClientProvider } from "@sveltestack/svelte-query";
+  import { HomePage } from "@app/components/pages";
+
+  const queryClient = new QueryClient();
+</script>
+
+<QueryClientProvider client={queryClient}>
+  <main>
+    <HomePage />
+  </main>
+</QueryClientProvider>
