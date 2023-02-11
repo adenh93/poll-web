6349c2131e8f6c2813cf756f043b7f1a20e5ef34
adenh93 <aden.herold1@gmail.com>
2023-02-11 21:10:16 +1100

    feat: added FormErrors component

---
+++ src/components/ui/FormErrors/FormErrors.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { ValidationMessage } from "@felte/reporter-svelte";
+
+  export let name: string;
+</script>
+
+<ValidationMessage for={name} let:messages {...$$restProps}>
+  {#if messages?.length}
+    <ul class="messages">
+      {#each messages as message}
+        <li>{message}</li>
+      {/each}
+    </ul>
+  {/if}
+</ValidationMessage>
+
+<style>
+  .messages {
+    margin: 0.8rem 0;
+    list-style: inside;
+  }
+
+  .messages li {
+    color: var(--status-error);
+    font-size: 1.3rem;
+  }
+</style>
