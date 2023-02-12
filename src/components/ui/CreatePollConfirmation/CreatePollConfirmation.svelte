22e552184b798b561ef5ef0d2666e730237d60c8
adenh93 <aden.herold1@gmail.com>
2023-02-12 21:05:49 +1100

    feat: added CreatePollConfirmation component

---
+++ src/components/ui/CreatePollConfirmation/CreatePollConfirmation.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import { Button, CopyToClipboard } from "@app/components/ui";
+
+  export let createdPoll: ApiTypes.CreatedPoll;
+
+  const dispatch = createEventDispatcher();
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const pollUrl = `${baseUrl}/polls/${createdPoll.id}`;
+
+  const createAnotherPoll = () => dispatch("resetForm");
+</script>
+
+<div class="container">
+  <h4>Success!</h4>
+  <p>
+    Your poll, '{createdPoll.name}' was created successfully! Copy the link
+    below and share it with your network so they can participate!
+  </p>
+  <CopyToClipboard text={pollUrl} />
+  <Button on:click={createAnotherPoll} type="button">
+    Create Another Poll
+  </Button>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.8rem;
+  }
+</style>
---
+++ src/components/ui/CreatePollConfirmation/index.ts
@@ -0,0 +1,1 @@
+export { default } from "./CreatePollConfirmation.svelte"
