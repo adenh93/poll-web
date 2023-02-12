10de3b21a3d8f2e35a74e919c29965deca368393
adenh93 <aden.herold1@gmail.com>
2023-02-12 21:08:39 +1100

    feat: added conditional rendering for CreatePollConfirmation and PollFormContainer to HomePage

--- src/components/pages/HomePage/HomePage.svelte
+++ src/components/pages/HomePage/HomePage.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
   import { MainLayout } from "@app/components/layouts";
   import { PollFormContainer } from "@app/components/containers";
-  import { Steps } from "@app/components/ui";
+  import { Steps, CreatePollConfirmation } from "@app/components/ui";
+
+  type CreatePollEventData = { receipt: ApiTypes.CreatedPoll };
+
+  let createdPoll: ApiTypes.CreatedPoll = null;
+
+  const onCreatePoll = (e: CustomEvent<CreatePollEventData>) => {
+    createdPoll = e.detail.receipt;
+  };
+
+  const onResetForm = (_e: CustomEvent) => {
+    createdPoll = null;
+  };
 </script>
 
 <MainLayout>
   <div class="primary-container" slot="primary">
     <h1>Create a Poll in a Few Easy Steps!</h1>
     <div class="form-card">
-      <PollFormContainer />
+      {#if createdPoll}
+        <CreatePollConfirmation {createdPoll} on:resetForm={onResetForm} />
+      {:else}
+        <PollFormContainer on:createPoll={onCreatePoll} />
+      {/if}
     </div>
   </div>
   <div class="secondary-container" slot="secondary">
