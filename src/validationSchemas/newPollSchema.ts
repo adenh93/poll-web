6e376ab3ca8a489c0f3679351a5e8a6123667243
adenh93 <aden.herold1@gmail.com>
2023-02-12 15:20:55 +1100

    feat: added default values for newPollSchema fields

--- src/validationSchemas/newPollSchema.ts
+++ src/validationSchemas/newPollSchema.ts
@@ -4,11 +4,13 @@ const newPollSchema = yup.object({
   name: yup.string()
     .min(5, "Poll name must be at least 5 characters.")
     .max(100, "Poll name cannot be more than 100 characters.")
-    .required("Enter a descriptive name."),
-  description: yup.string().nullable(),
+    .required("Enter a descriptive name.")
+    .default(""),
+  description: yup.string().nullable().default(null),
   endDate: yup
     .string()
-    .required("An end date must be provided when creating a poll."),
+    .required("An end date must be provided when creating a poll.")
+    .default(""),
   choices: yup
     .array()
     .min(2, "You must provide at least 2 choices.")
@@ -18,9 +20,10 @@ const newPollSchema = yup.object({
       yup.object({
         name: yup
           .string()
-          .required("Enter a descriptive name for this choice."),
+          .required("Enter a descriptive name for this choice.")
+          .default("")
       })
-    ),
+    )
 });
 
 export type NewPollSchema = yup.InferType<typeof newPollSchema>;
