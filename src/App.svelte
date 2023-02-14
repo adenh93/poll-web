671d14c90db3e3be86ebef56b6f494f8b2492306
adenh93 <aden.herold1@gmail.com>
2023-02-14 23:55:33 +1100

    feat: configured svelte-routing router in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
   import { QueryClient, QueryClientProvider } from "@sveltestack/svelte-query";
-  import { HomePage } from "@app/components/pages";
+  import { Router, Route } from "svelte-routing";
+  import { HomePage, PollPage } from "@app/components/pages";
 
   const queryClient = new QueryClient();
 </script>
 
 <QueryClientProvider client={queryClient}>
-  <main>
-    <HomePage />
-  </main>
+  <Router>
+    <main>
+      <Route path="/polls/:id" let:params>
+        <PollPage id={params.id} />
+      </Route>
+      <Route path="/">
+        <HomePage />
+      </Route>
+    </main>
+  </Router>
 </QueryClientProvider>
