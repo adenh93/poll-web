2576819f23bbbb59a325db5acce63909836b4ca0
adenh93 <aden.herold1@gmail.com>
2023-02-14 21:22:54 +1100

    feat: added PollChoice component

---
+++ src/components/ui/PollChoice/PollChoice.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { RadioButton } from "@app/components/ui";
+
+  export let checked: boolean;
+</script>
+
+<div class="container">
+  <RadioButton {checked} on:change>
+    <slot />
+  </RadioButton>
+</div>
+
+<style>
+  .container {
+    padding: 1.2rem 0.8rem;
+    background-color: var(--bg-shaded);
+    border-radius: 0.5rem;
+  }
+</style>
