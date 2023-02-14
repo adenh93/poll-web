fa1f73053617cd76bbe642883708847377b53ecb
adenh93 <aden.herold1@gmail.com>
2023-02-14 23:49:27 +1100

    feat: added PollPage page component

---
+++ src/components/pages/PollPage/PollPage.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { SecondaryLayout } from "@app/components/layouts";
+  import { PollContainer } from "@app/components/containers";
+
+  export let id: string;
+</script>
+
+<SecondaryLayout>
+  <div class="primary-container">
+    <PollContainer {id} />
+  </div>
+</SecondaryLayout>
+
+<style>
+  .primary-container {
+    display: flex;
+    justify-content: center;
+  }
+</style>
