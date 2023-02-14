ec2a91394ce9d4ed3de9e8f7fa0502f9aeae0bdc
adenh93 <aden.herold1@gmail.com>
2023-02-14 23:51:53 +1100

    feat: added Poll component

--- src/components/ui/Poll/Poll.stories.ts
+++ src/components/ui/Poll/Poll.stories.ts
@@ -1,5 +1,20 @@
 import Poll from './Poll.svelte';
 
+const pollProps = {
+  id: "123e4567-e89b-12d3-a456-426614174000",
+  name: "What should be my next video idea?",
+  description: "I've decided to put out some more programming content on Youtube, so I've created this poll to gather some ideas from the community.",
+  endDate: "2023-02-24T12:30:00.000-00:00",
+  choices: [
+    { id: "choice-1", name: "Rewriting open source project with Rust" },
+    { id: "choice-2", name: "Resolving issues in popular open source projects" },
+    { id: "choice-3", name: "Contribute to Helix editor" },
+    { id: "choice-4", name: "Build an emulator core/frontend (Chip8 or Atari2600)" },
+    { id: "choice-5", name: "Try out some WebAssembly front-end development" },
+    { id: "choice-6", name: "Vim configuration" }
+  ]
+}
+
 export default {
   component: Poll,
   title: "Poll",
@@ -10,21 +25,28 @@ const Template = (args: any) => ({
   props: args
 });
 
-export const Default = Template.bind({});
+export const NoResults = Template.bind({});
 
-Default.args = {
+NoResults.args = {
   poll: {
-    id: "123e4567-e89b-12d3-a456-426614174000",
-    name: "What should be my next video idea?",
-    description: "I've decided to put out some more programming content on Youtube, so I've created this poll to gather some ideas from the community.",
-    endDate: "2023-02-24T12:30:00.000-00:00",
-    choices: [
-      { id: "choice-1", name: "Rewriting open source project with Rust" },
-      { id: "choice-2", name: "Resolving issues in popular open source projects" },
-      { id: "choice-3", name: "Contribute to Helix editor" },
-      { id: "choice-4", name: "Build an emulator core/frontend (Chip8 or Atari2600)" },
-      { id: "choice-5", name: "Try out some WebAssembly front-end development" },
-      { id: "choice-6", name: "Vim configuration" }
+    ...pollProps
+  },
+  hasResults: false,
+}
+
+export const WithResults = Template.bind({});
+
+WithResults.args = {
+  poll: {
+    ...pollProps,
+    results: [
+      { choiceId: "choice-1", voteCount: 35 },
+      { choiceId: "choice-2", voteCount: 58 },
+      { choiceId: "choice-3", voteCount: 14 },
+      { choiceId: "choice-4", voteCount: 42 },
+      { choiceId: "choice-5", voteCount: 8 },
+      { choiceId: "choice-6", voteCount: 21 },
     ]
-  }
+  },
+  hasResults: true
 }
--- src/components/ui/Poll/Poll.svelte
+++ src/components/ui/Poll/Poll.svelte
@@ -1,41 +1,16 @@
 <script lang="ts">
-  import { Button, PollChoice } from "@app/components/ui";
+  import { PollResults, VotePoll } from "@app/components/ui";
 
   export let poll: ApiTypes.Poll;
-
-  let selectedChoice: string = null;
-
-  const formattedEndDate = new Intl.DateTimeFormat("en-AU", {
-    dateStyle: "full",
-    timeStyle: "long",
-  }).format(new Date(poll.endDate));
-
-  const onSelectChoice = (id: string) => {
-    selectedChoice = id;
-  };
+  export let hasResults: boolean;
 </script>
 
 <div class="poll-card">
-  <div class="content">
-    <h3>{poll.name}</h3>
-    {#if poll.description}
-      <p>{poll.description}</p>
-    {/if}
-    <p class="end-date">Ends on {formattedEndDate}</p>
-    <div class="choices">
-      {#each poll.choices as choice}
-        <PollChoice
-          checked={selectedChoice === choice.id}
-          on:change={() => onSelectChoice(choice.id)}
-        >
-          {choice.name}
-        </PollChoice>
-      {/each}
-    </div>
-  </div>
-  <div class="footer">
-    <Button type="button">Vote</Button>
-  </div>
+  {#if hasResults}
+    <PollResults {poll} />
+  {:else}
+    <VotePoll {poll} />
+  {/if}
 </div>
 
 <style>
@@ -44,26 +19,5 @@
     background-color: var(--bg-white);
     border-radius: 0.5rem;
     padding: 4rem 5rem;
-    display: flex;
-    flex-direction: column;
-    row-gap: 8rem;
-    justify-content: space-between;
-  }
-
-  .poll-card > .content {
-    display: flex;
-    flex-direction: column;
-    row-gap: 2.2rem;
-  }
-
-  .content > .end-date {
-    font-size: 1.4rem;
-    font-weight: 600;
-  }
-
-  .content > .choices {
-    display: flex;
-    flex-direction: column;
-    row-gap: 1rem;
   }
 </style>
