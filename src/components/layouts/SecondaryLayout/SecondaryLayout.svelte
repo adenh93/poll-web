270351e44c93795274326b5110402fd0ace0fe6f
adenh93 <aden.herold1@gmail.com>
2023-02-14 23:46:55 +1100

    feat: added SecondaryLayout layout component

---
+++ src/components/layouts/SecondaryLayout/SecondaryLayout.svelte
@@ -0,0 +1,19 @@
+<div class="container">
+  <div class="primary-section">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .container {
+    padding: 1.5rem;
+  }
+
+  .primary-section {
+    background: var(--gradient-bg), url("/assets/images/background.jpg");
+    background-size: cover;
+    border-radius: 0.5rem;
+    padding: 10rem 3rem;
+    min-height: 70rem;
+  }
+</style>
